ac8c0ff9ab2c07cb17608af095ea9fb30ab669e0
ibtehal2122 <ibtehalashraf@gmail.com>
2026-02-07 20:36:31 +0000

    shorten node group name

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -26,7 +26,7 @@ module "eks" {
   # Node Groups
   eks_managed_node_groups = {
     main = {
-      name = "${var.cluster_name}-node-group"
+      name = "${var.cluster_name}-node"
 
       instance_types = var.instance_types
       min_size       = var.min_size
